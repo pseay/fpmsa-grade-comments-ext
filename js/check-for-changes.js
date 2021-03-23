350ed3461b0f45eefafa3e649670fb2201c32605
Preston Seay <preston.seay@outlook.com>
2021-03-23 00:08:03 -0700

    get current MarkingPeriodId accurately

--- js/check-for-changes.js
+++ js/check-for-changes.js
@@ -15,11 +15,42 @@ function useData(data) {
 }
 
 function getData() {
-    fetch("https://francisparker.myschoolapp.com/api/datadirect/GradeBookPerformanceAssignmentStudentList/?sectionId=24132279&markingPeriodId=9596&studentUserId=4667430")
-        .then(response => response.json())
-        .then(data => {
-            useData(data);
+    fetch(
+        'https://francisparker.myschoolapp.com/api/datadirect/GradeBookPerformanceAssignmentStudentList/?sectionId=24132279&markingPeriodId=9596&studentUserId=4667430'
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            // useData(data);
+            console.log(data);
         });
+}
+
+async function getCurrentMarkingPeriod() {
+    //context url gives lots of information about the student
+    //has sections: UserInfo, Tasks, Groups (classes), Directories, Calendars, and InboxDirectories
+    const contextUrl = 'https://francisparker.myschoolapp.com/api/webapp/context';
+    const markingPeriodUrl = 'https://francisparker.myschoolapp.com/api/gradebook/GradeBookMyDayMarkingPeriods';
+    let currentSectionIds = await fetch(contextUrl)
+        .then((response) => response.json())
+        .then((data) => data['Groups'].map((group) => group['CurrentSectionId']));
+    let queryParam = [{
+        DurationId: 128016,//TODO: find where this is from
+        LeadSectionList: currentSectionIds.map(id => {
+            return { LeadSectionId: id };
+        })
+    }]
+    //gets the markingPeriodUrl endpoint with query params
+    return (
+        fetch(
+            markingPeriodUrl + '?durationSectionList=' +
+            JSON.stringify(queryParam) +
+            '&userId=4667430' +
+            '&personaId=2'
+        )
+            .then((response) => response.json())
+            //comes in format: [{ ...info, MarkingPeriodId: xxxx }]
+            .then((data) => console.log(data[0].MarkingPeriodId))
+    );
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
