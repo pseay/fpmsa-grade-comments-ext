96f0c8acb4f219028355413b8dfd6181d2bf0e59
Preston Seay <preston.seay@outlook.com>
2021-03-25 14:51:03 -0700

    gets more data -> cache

--- js/background.js
+++ js/background.js
@@ -2,8 +2,8 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status === "complete") {
         //the page has finished loading
         chrome.tabs.sendMessage(tabId, {
-            extension: "comment-retriever",
-            info: "page loaded"
+            extension: 'grade-comment-monitor',
+            info: 'page loaded',
         });
     }
 });
--- js/check-for-changes.js
+++ js/check-for-changes.js
@@ -1,4 +1,5 @@
 function useData(data) {
+    console.log("called!!!");
     data = data.map(dp => {
         function removeTags(taggedText) {
             let span = document.createElement("span");
@@ -11,50 +12,116 @@ function useData(data) {
         }
         return {'id': dp.AssignmentId, 'comment': removeTags(dp.AdditionalInfo)};
     });
-    console.log(data);
 }
 
-function getData() {
-    fetch(
-        'https://francisparker.myschoolapp.com/api/datadirect/GradeBookPerformanceAssignmentStudentList/?sectionId=24132279&markingPeriodId=9596&studentUserId=4667430'
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            // useData(data);
-            console.log(data);
-        });
-}
-
-async function getCurrentMarkingPeriod() {
-    //context url gives lots of information about the student
-    //has sections: UserInfo, Tasks, Groups (classes), Directories, Calendars, and InboxDirectories
-    const contextUrl = 'https://francisparker.myschoolapp.com/api/webapp/context';
-    const markingPeriodUrl = 'https://francisparker.myschoolapp.com/api/gradebook/GradeBookMyDayMarkingPeriods';
-    let currentSectionIds = await fetch(contextUrl)
-        .then((response) => response.json())
-        .then((data) => data['Groups'].map((group) => group['CurrentSectionId']));
-    let queryParam = [{
-        DurationId: 128016,//TODO: find where this is from
-        LeadSectionList: currentSectionIds.map(id => {
-            return { LeadSectionId: id };
-        })
-    }]
-    //gets the markingPeriodUrl endpoint with query params
-    return (
+async function main() {
+    /*let funcs = getFromCache("currentClassesSectionIds").map(sectionId => () => {
         fetch(
-            markingPeriodUrl + '?durationSectionList=' +
-            JSON.stringify(queryParam) +
-            '&userId=4667430' +
-            '&personaId=2'
+        'https://francisparker.myschoolapp.com/api/datadirect/GradeBookPerformanceAssignmentStudentList/?' + 
+        'sectionId=' + sectionId +
+        '&markingPeriodId=' + getFromCache("markingPeriodId") +
+        '&studentUserId=' + getFromCache("userId")
         )
             .then((response) => response.json())
-            //comes in format: [{ ...info, MarkingPeriodId: xxxx }]
-            .then((data) => console.log(data[0].MarkingPeriodId))
-    );
+            .then((data) => {
+                useData(data);
+            });
+    });*/
+    console.log(funcs);
 }
 
+async function getLongQueryParameter() {
+    const contextUrl = 'https://francisparker.myschoolapp.com/api/webapp/context';
+    let data = await fetch(contextUrl)
+        .then((response) => response.json());
+    let uid = data.UserInfo?.UserId;
+    if (uid) {
+        setToCache("userId", uid);
+    }
+    let currentSectionIds = data['Groups'].map((group) => group['CurrentSectionId']);
+    let queryParam = [
+        {
+            DurationId: getFromCache("durationId"),
+            LeadSectionList: currentSectionIds.map((id) => {
+                return { LeadSectionId: id };
+            }),
+        },
+    ];
+    return queryParam;
+}
+
+//parses 'Progress' page for its data and sets localStorage "cache"
+async function updateProgressPageData() {
+    //gets durationId which is needed for getting the currentMarkingPeriod
+    //* --Getting the Current Duration ID--
+    // getting all the durationIds first
+    let id = setInterval(function() {
+        let triLabels = document.querySelectorAll("[data-action='term']");
+        if (triLabels.length >= 3) {
+            let ids = [...triLabels].map(label => label.dataset.value);
+            clearInterval(id);
+            useIds(ids);
+        }
+    }, 1000);
+    // check each durationIds for currentMarkingPeriod
+    async function useIds(ids) {
+
+        const markingPeriodUrl = 'https://francisparker.myschoolapp.com/api/gradebook/GradeBookMyDayMarkingPeriods';
+
+        let queryParam = await getLongQueryParameter();
+
+        for (let id of ids) {
+            queryParam[0].DurationId=id;
+            let queryParams = '?durationSectionList=' + JSON.stringify(queryParam) + '&userId=4667430' + '&personaId=2';
+            console.log(markingPeriodUrl + queryParams);
+            let data = await fetch( markingPeriodUrl + queryParams )
+                .then((response) => response.json());
+            let cur = data[0].CurrentMarkingPeriod;
+            if (cur) {
+                //setting it in the local storage
+                setToCache("durationId", id);
+                setToCache("markingPeriodId", data[0].MarkingPeriodId);
+                break;
+            }
+        };
+    }
+}
+
+//runs requests for each page load
+async function updateMiscData() {
+    const contextUrl = 'https://francisparker.myschoolapp.com/api/webapp/context';
+    let groups = await fetch(contextUrl)
+        .then(res => res.json())
+        .then(data => data["Groups"]);
+    groups = groups.filter(group => {
+        return group["Association"] === 1          //class, not club or other
+            && group["CurrentEnrollment"] === true //current trimester (3 classes -> 1 class)
+            && group["Category"] !== "US-Utility"; //gets rid of free blocks
+    });
+    let sectionIds = groups.map(group => group["LeadSectionId"]);
+    setToCache("currentClassesSectionIds", sectionIds);
+}
+
+//cache reading and writing
+//uses localStorage
+function setToCache(name, value) {
+    let gcCache = JSON.parse(localStorage.getItem('grade-comments-cache') || "{}");
+    gcCache[name] = value;
+    localStorage.setItem('grade-comments-cache', JSON.stringify(gcCache));
+}
+function getFromCache(name) {
+    let gcCache = JSON.parse(localStorage.getItem('grade-comments-cache') || "{}");
+    return gcCache[name];
+}
+
+//message listener
+//receives message from background.js script
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request?.extension === 'comment-retriever') {
-        getData();
+    if (request?.extension === 'grade-comment-monitor') {
+        main();
+        if (location.href.includes('#studentmyday/progress')) {
+            updateProgressPageData();
+        }
+        updateMiscData();
     }
 });
