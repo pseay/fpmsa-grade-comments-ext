a3c0f3731525c5a97a0b9ffed814ceab16d26c69
Preston Seay <preston.seay@outlook.com>
2021-04-15 23:41:40 -0700

    saving comment ready - noficiations

--- js/check-for-changes.js
+++ js/check-for-changes.js
@@ -1,33 +1,75 @@
-function useData(data) {
-    console.log("called!!!");
-    data = data.map(dp => {
-        function removeTags(taggedText) {
-            let span = document.createElement("span");
+function getCurrentMillis() {
+    return Date.parse(new Date());
+}
+
+function removeTags(taggedText) {
+    let span = document.createElement("span");
             span.innerHTML= taggedText;
             let text = span.textContent;
-            return hashCode(text);
-        };
-        function hashCode(s){
-            return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
-        }
+            return text;
+}
+
+function hashCode(text) {
+    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+}
+
+function useData(data) {
+    data = data.map(dp => {
+        //TODO: set to id lookup
+        //addToCacheCache("id-lookup", id, title)
         return {'id': dp.AssignmentId, 'comment': removeTags(dp.AdditionalInfo)};
     });
+    let changePeriodEnd = getCurrentMillis();
+    let changePeriodStart = getFromCache("change-period-start") || null;
+    data = data.map(dp => { return {
+        ...dp,
+        "change-period-start": changePeriodStart,
+        "change-period-end": changePeriodEnd
+    }});
+    let commentHistory = getFromCache("comment-history");
+    let unmarkedData = [];
+    for (const dp of data) {
+        //TODO: generate notification list
+        //-- set to cache as text messages?
+        let previousComments = commentHistory.filter(chdp => chdp.id === dp.id);
+        if (previousComments.length === 0) {
+            //there is no previous record, so add the current data
+            unmarkedData.push(dp);
+            //* new grade and ?comment
+        } else {
+            let recentRecord = previousComments[previousComments.length-1];
+            if (recentRecord.comment !== dp.comment) {
+                //the comment record is not up to date, so add the current data
+                unmarkedData.push(dp);
+                //* new comment
+                //* get previous comment and show change "... -> ..."
+            }
+        }
+        //else, the prior info has covered this
+    }
+    //updating localStorage
+    commentHistory = [...commentHistory, ...unmarkedData];
+    setToCache("comment-history", commentHistory);
 }
 
 async function main() {
-    /*let funcs = getFromCache("currentClassesSectionIds").map(sectionId => () => {
-        fetch(
-        'https://francisparker.myschoolapp.com/api/datadirect/GradeBookPerformanceAssignmentStudentList/?' + 
-        'sectionId=' + sectionId +
-        '&markingPeriodId=' + getFromCache("markingPeriodId") +
-        '&studentUserId=' + getFromCache("userId")
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                useData(data);
-            });
-    });*/
-    console.log(funcs);
+    let funcs = getFromCache("currentClassesSectionIds").map(sectionId => {
+        return async() => {
+            fetch(
+            'https://francisparker.myschoolapp.com/api/datadirect/GradeBookPerformanceAssignmentStudentList/?' + 
+            'sectionId=' + sectionId +
+            '&markingPeriodId=' + getFromCache("markingPeriodId") +
+            '&studentUserId=' + getFromCache("userId")
+            )
+                .then((response) => response.json())
+                .then((data) => {
+                    useData(data);
+                });
+        };
+    });
+    funcs.forEach(func => func());
+    await Promise.all(funcs);
+    setToCache('last-update', getCurrentMillis());
 }
 
 async function getLongQueryParameter() {
@@ -73,7 +115,6 @@ async function updateProgressPageData() {
         for (let id of ids) {
             queryParam[0].DurationId=id;
             let queryParams = '?durationSectionList=' + JSON.stringify(queryParam) + '&userId=4667430' + '&personaId=2';
-            console.log(markingPeriodUrl + queryParams);
             let data = await fetch( markingPeriodUrl + queryParams )
                 .then((response) => response.json());
             let cur = data[0].CurrentMarkingPeriod;
@@ -112,6 +153,20 @@ function setToCache(name, value) {
 function getFromCache(name) {
     let gcCache = JSON.parse(localStorage.getItem('grade-comments-cache') || "{}");
     return gcCache[name];
+}
+//sub cache reading and writing
+function addToCacheCache(name1, name2, value) {
+    let gcCache = JSON.parse(localStorage.getItem('grade-comments-cache') || "{}");
+    let subCache = gcCache[name1] || {};
+
+    subCache[name2] = value;
+    gcCache[name1] = subCache;
+    localStorage.setItem('grade-comments-cache', JSON.stringify(gcCache));
+}
+function getFromCacheCache(name1, name2) {
+    let gcCache = JSON.parse(localStorage.getItem('grade-comments-cache') || "{}");
+    let subCache = gcCache[name1] || {};
+    return subCache[name2];
 }
 
 //message listener
