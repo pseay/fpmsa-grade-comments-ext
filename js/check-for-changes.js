57e882815f4fddc224fdfe43a5cecbb655373592
Preston Seay <preston.seay@outlook.com>
2021-05-03 22:59:09 -0700

    fixed bugs. info_panel works

--- js/check-for-changes.js
+++ js/check-for-changes.js
@@ -1,36 +1,51 @@
+const LOCAL_STORAGE_KEY = 'grade-comment-storage';
+
 function getCurrentMillis() {
     return Date.parse(new Date());
 }
 
 function removeTags(taggedText) {
-    let span = document.createElement("span");
-    span.innerHTML= taggedText;
+    let span = document.createElement('span');
+    span.innerHTML = taggedText;
     let text = span.textContent;
     return text;
 }
 
 function hashCode(text) {
-    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+    return s.split('').reduce(function (a, b) {
+        a = (a << 5) - a + b.charCodeAt(0);
+        return a & a;
+    }, 0);
 }
 
 function loadInfoPage() {
-    let iId = setInterval(function() {
-        if (location.href.includes("#gc/info") == false) {
+    let iId = setInterval(function () {
+        if (location.href.includes('#gc/info') == false) {
             clearInterval(iId);
             return;
         }
-        let loaded = document.querySelector("#site-header-container") != null;
+        let loaded = document.querySelector('#site-header-container') != null;
         if (loaded) {
-            document.querySelector("#skyui-header").outerHTML = "";
-            document.querySelector("#site-top-spacer").outerHTML = "";
+            document.querySelector('#skyui-header').outerHTML = '';
+            document.querySelector('#site-top-spacer').outerHTML = '';
 
+            let infoPanel = document.createElement('div');
+            infoPanel.setAttribute('style', 'width: 100%; height: calc(100vh - 100px); margin-top: 100px;');
+            infoPanel.setAttribute('id', 'gc-info-panel');
+            document.querySelector('#app').children[0].appendChild(infoPanel);
+
+            showInfoPage(infoPanel);
             clearInterval(iId);
         }
-    }, 1000);
+    }, 100);
+}
+
+async function showInfoPage(info_panel) {
+    info_panel.innerHTML  =  "<h1 style='color: blue;'>Hi, info panel!</h1>";
 }
 
 function useData(data) {
-    if (!data) return;
+    if (!data || !data?.map) return;
     data = data.map((dp) => {
         let idVal = {
             title: removeTags(dp.AssignmentShortDescription),
@@ -39,6 +54,7 @@ function useData(data) {
         addToCacheCache('id-lookup', dp.AssignmentId, idVal);
         return { id: dp.AssignmentId, comment: removeTags(dp.AdditionalInfo), points: dp.Points };
     });
+    //adding the change period start millis - end millis
     let changePeriodEnd = getCurrentMillis();
     let changePeriodStart = getFromCache('last-update') || null;
     data = data.map((dp) => {
@@ -50,14 +66,14 @@ function useData(data) {
     });
     let commentHistory = getFromCache('comment-history') || [];
     let unmarkedData = [];
-    let notifications = [];
+    let newNotifications = [];
     for (const dp of data) {
         let previousComments = commentHistory.filter((chdp) => chdp.id === dp.id);
         if (previousComments.length === 0) {
             //there is no previous record, so add the current data
             unmarkedData.push(dp);
             //making notification
-            notifications.push({ ...dp, new: true });
+            newNotifications.push(dp.id);
         } else {
             const recentRecord = previousComments[previousComments.length - 1];
             const commentChange = recentRecord.comment !== dp.comment;
@@ -66,27 +82,28 @@ function useData(data) {
                 //the comment/points record is not up to date, so add the current data
                 unmarkedData.push(dp);
                 //making notification
-                const prevComment = commentChange ? { comment: recentRecord.comment } : {};
-                const prevPoints = pointsChange ? { points: recentRecord.points } : {};
-                notifications.push({ ...dp, new: false, ...prevComment, ...prevPoints });
+                //// const prevComment = commentChange ? { p_comment: recentRecord.comment } : {};
+                //// const prevPoints = pointsChange ? { p_points: recentRecord.points } : {};
+                newNotifications.push(dp.id);
             }
         }
         //else, the prior info has covered this
     }
-    let prev_notifs = getFromCache('notifications') || [];
-    setToCache('notifications', [...prev_notifs, ...notifications]);
+    let notifications = getFromCache('notifications') || [];
+    setToCache('notifications', [...notifications, ...newNotifications]);
     //updating localStorage
     commentHistory = [...commentHistory, ...unmarkedData];
-    setToCache("comment-history", commentHistory);
+    setToCache('comment-history', commentHistory);
 }
 
 async function main() {
-    let ten_min = 600000;
-    if (getCurrentMillis() - getFromCache('last-update') < ten_min) {
+    let fiveMin = 300000;
+    if (getCurrentMillis() - getFromCache('last-update') < fiveMin) {
         return;
     }
-    let funcs = getFromCache('currentClassesSectionIds').map((sectionId) => {
-        return new Promise(async  (resolve, reject) => {
+    let sectionIds = getFromCache('currentClassesSectionIds') || [];
+    let funcs = sectionIds.map((sectionId) => {
+        return new Promise(async (resolve, reject) => {
             await fetch(
                 'https://francisparker.myschoolapp.com/api/datadirect/GradeBookPerformanceAssignmentStudentList/?' +
                     'sectionId=' +
@@ -110,16 +127,15 @@ async function main() {
 
 async function getLongQueryParameter() {
     const contextUrl = 'https://francisparker.myschoolapp.com/api/webapp/context';
-    let data = await fetch(contextUrl)
-        .then((response) => response.json());
+    let data = await fetch(contextUrl).then((response) => response.json());
     let uid = data.UserInfo?.UserId;
     if (uid) {
-        setToCache("userId", uid);
+        setToCache('userId', uid);
     }
     let currentSectionIds = data['Groups'].map((group) => group['CurrentSectionId']);
     let queryParam = [
         {
-            DurationId: getFromCache("durationId"),
+            DurationId: getFromCache('durationId'),
             LeadSectionList: currentSectionIds.map((id) => {
                 return { LeadSectionId: id };
             }),
@@ -133,34 +149,32 @@ async function updateProgressPageData() {
     //gets durationId which is needed for getting the currentMarkingPeriod
     //* --Getting the Current Duration ID--
     // getting all the durationIds first
-    let id = setInterval(function() {
+    let id = setInterval(function () {
         let triLabels = document.querySelectorAll("[data-action='term']");
         if (triLabels.length >= 3) {
-            let ids = [...triLabels].map(label => label.dataset.value);
+            let ids = [...triLabels].map((label) => label.dataset.value);
             clearInterval(id);
             useIds(ids);
         }
-    }, 1000);
+    }, 100);
     // check each durationIds for currentMarkingPeriod
     async function useIds(ids) {
-
         const markingPeriodUrl = 'https://francisparker.myschoolapp.com/api/gradebook/GradeBookMyDayMarkingPeriods';
 
         let queryParam = await getLongQueryParameter();
 
         for (let id of ids) {
-            queryParam[0].DurationId=id;
+            queryParam[0].DurationId = id;
             let queryParams = '?durationSectionList=' + JSON.stringify(queryParam) + '&userId=4667430' + '&personaId=2';
-            let data = await fetch( markingPeriodUrl + queryParams )
-                .then((response) => response.json());
+            let data = await fetch(markingPeriodUrl + queryParams).then((response) => response.json());
             let cur = data[0].CurrentMarkingPeriod;
             if (cur) {
                 //setting it in the local storage
-                setToCache("durationId", id);
-                setToCache("markingPeriodId", data[0].MarkingPeriodId);
+                setToCache('durationId', id);
+                setToCache('markingPeriodId', data[0].MarkingPeriodId);
                 break;
             }
-        };
+        }
     }
 }
 
@@ -168,39 +182,41 @@ async function updateProgressPageData() {
 async function updateMiscData() {
     const contextUrl = 'https://francisparker.myschoolapp.com/api/webapp/context';
     let groups = await fetch(contextUrl)
-        .then(res => res.json())
-        .then(data => data["Groups"]);
-    groups = groups.filter(group => {
-        return group["Association"] === 1          //class, not club or other
-            && group["CurrentEnrollment"] === true //current trimester (3 classes -> 1 class)
-            && group["Category"] !== "US-Utility"; //gets rid of free blocks
+        .then((res) => res.json())
+        .then((data) => data['Groups']);
+    groups = groups.filter((group) => {
+        return (
+            group['Association'] === 1 && //class, not club or other
+            group['CurrentEnrollment'] === true && //current trimester (3 classes -> 1 class)
+            group['Category'] !== 'US-Utility'
+        ); //gets rid of free blocks
     });
-    let sectionIds = groups.map(group => group["LeadSectionId"]);
-    setToCache("currentClassesSectionIds", sectionIds);
+    let sectionIds = groups.map((group) => group['LeadSectionId']);
+    setToCache('currentClassesSectionIds', sectionIds);
 }
 
 //cache reading and writing
 //uses localStorage
 function setToCache(name, value) {
-    let gcCache = JSON.parse(localStorage.getItem('grade-comments-cache') || "{}");
+    let gcCache = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
     gcCache[name] = value;
-    localStorage.setItem('grade-comments-cache', JSON.stringify(gcCache));
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(gcCache));
 }
 function getFromCache(name) {
-    let gcCache = JSON.parse(localStorage.getItem('grade-comments-cache') || "{}");
+    let gcCache = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
     return gcCache[name];
 }
 //sub cache reading and writing
 function addToCacheCache(name1, name2, value) {
-    let gcCache = JSON.parse(localStorage.getItem('grade-comments-cache') || "{}");
+    let gcCache = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
     let subCache = gcCache[name1] || {};
 
     subCache[name2] = value;
     gcCache[name1] = subCache;
-    localStorage.setItem('grade-comments-cache', JSON.stringify(gcCache));
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(gcCache));
 }
 function getFromCacheCache(name1, name2) {
-    let gcCache = JSON.parse(localStorage.getItem('grade-comments-cache') || "{}");
+    let gcCache = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
     let subCache = gcCache[name1] || {};
     return subCache[name2];
 }
@@ -216,7 +232,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         if (location.href.includes('#studentmyday/progress')) {
             updateProgressPageData();
         }
-        if (location.href.includes("#gc/info")) {
+        if (location.href.includes('#gc/info')) {
             loadInfoPage();
         }
         updateMiscData();
