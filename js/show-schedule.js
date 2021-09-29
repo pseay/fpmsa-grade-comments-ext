fa4ae9455af5aaaa5f0ee9aa9cf3440fd698b63b
Preston Seay <preston.seay@outlook.com>
2021-09-29 09:14:54 -0700

    working for other assignments too

--- js/show-schedule.js
+++ js/show-schedule.js
@@ -236,7 +236,7 @@ function addMissingAssignments(schedule) {
                     span.style.setProperty('background-color', '#B7DA9B');
                 }
             };
-            span.innerText = assignment.details;
+            span.innerText = assignment.name;
             div2.appendChild(span);
         });
         div.appendChild(div2);
