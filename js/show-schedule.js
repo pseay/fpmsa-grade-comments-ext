a0f502c5bc1a887831b0125a08601f4a7f4f95e9
Preston Seay <preston.seay@outlook.com>
2021-12-12 22:14:11 -0800

    up-to-date

--- js/show-schedule.js
+++ js/show-schedule.js
@@ -98,7 +98,7 @@ async function getData() {
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-			if (!data) return;
+			if (!data || !data.map) return;
             let assignments = data.map((assignment) => {
                 return {
                     class: assignment.groupname,
